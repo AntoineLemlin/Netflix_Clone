28c3089c4590764ae70bd58b4b4988cbc2ea4ab2
AntoineLemlin <ant.lemlin@gmail.com>
2021-12-08 16:45:10 +0100

    netflix design

---
+++ src/components/Row.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import axios from "../axios";
+import "../styles/Row.css";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+const Row = (props) => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(props.fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [props.fetchUrl]);
+
+  return (
+    <div className="row">
+      <h2>{props.title}</h2>
+      <div className="row_posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row_poster ${props.isLargeRow && "row_posterLarge"}`}
+            src={`${base_url}${
+              props.isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Row;
