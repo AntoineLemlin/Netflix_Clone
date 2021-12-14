58c78abd4577cf2619dd485010812319fa454691
AntoineLemlin <ant.lemlin@gmail.com>
2021-12-14 14:15:05 +0100

    trailer

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "../axios";
 import "../styles/Row.css";
 import Loader from "react-loader-spinner";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 const Row = (props) => {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -23,6 +26,27 @@ const Row = (props) => {
     setLoading(false);
   }, [props.fetchUrl]);
 
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
   return (
     <div className="row">
       <h2>{props.title}</h2>
@@ -31,6 +55,7 @@ const Row = (props) => {
           movies.map((movie) => (
             <img
               key={movie.id}
+              onClick={() => handleClick(movie)}
               className={`row_poster ${props.isLargeRow && "row_posterLarge"}`}
               src={`${base_url}${
                 props.isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -49,6 +74,7 @@ const Row = (props) => {
           />
         )}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 };
